0b35306f66925d6dffcd38d049b0e38ab60e8b37
Lord Jerry <lovinjerry004@gmail.com>
2020-12-11 00:04:57 +0100

    install chakra

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,34 @@
 import React, { Suspense } from 'react';
 import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { ChakraProvider } from '@chakra-ui/react';
 
 import Layout from './Layout';
 import store from './redux/store';
 import history from './history';
 import Routes from './routes';
 
-import Login from './pages/Login';
-import Home from './pages/Home';
-import Dashboard from './pages/Dashboard';
-import Portfolio from './pages/Portfolio';
-import Portfolios from './pages/Portfolios';
-import Footer from './components/mini-components/Footer';
-import SignUp from './pages/SignUp';
-import Pricing from './pages/Pricing';
+// import Login from './pages/Login';
+// import Home from './pages/Home';
+// import Dashboard from './pages/Dashboard';
+// import Portfolio from './pages/Portfolio';
+// import Portfolios from './pages/Portfolios';
+// import Footer from './components/mini-components/Footer';
+// import SignUp from './pages/SignUp';
+// import Pricing from './pages/Pricing';
 
 export default function App() {
     return (
-      <Provider store={store}>
+        <Provider store={store}>
             <Router history={history}>
-                <Layout>
-                    <Suspense fallback="loading">
-                        <Routes />
-                  </Suspense>
-              </Layout>
-        </Router>
+                <ChakraProvider>
+                    <Layout>
+                        <Suspense fallback="loading">
+                            <Routes />
+                        </Suspense>
+                    </Layout>
+                </ChakraProvider>
+            </Router>
         </Provider>
     );
 }
